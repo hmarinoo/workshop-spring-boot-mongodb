4ad75b86f80dad8c5bea1ab17b359080eeaf9c8b
hmarinoo <hmarinoo@gmail.com>
2019-03-03 18:04:53 -0300

    Projection of author data with DTO

--- src/main/java/com/hmarinoo/workshopmongo/config/Instatiation.java
+++ src/main/java/com/hmarinoo/workshopmongo/config/Instatiation.java
@@ -2,15 +2,15 @@ package com.hmarinoo.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import com.hmarinoo.workshopmongo.domain.Post;
 import com.hmarinoo.workshopmongo.domain.User;
+import com.hmarinoo.workshopmongo.dto.AuthorDTO;
+import com.hmarinoo.workshopmongo.domain.Post;
 import com.hmarinoo.workshopmongo.repository.PostRepository;
 import com.hmarinoo.workshopmongo.repository.UserRepository;
 @Configuration
@@ -27,15 +27,16 @@ public class Instatiation implements CommandLineRunner {
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		Post morning = new Post(null,sdf.parse("12/01/2019"),"Hello","Hello people good morning",maria);
-		Post afternoon = new Post(null,sdf.parse("10/02/2019"),"Good afternoon","Hello friends good afternoon",alex);
-		Post night = new Post(null,sdf.parse("10/10/2018"),"Nice nigth","Good night mi friends",bob);
+		Post morning = new Post(null,sdf.parse("12/01/2019"),"Hello","Hello people good morning",new AuthorDTO(maria));
+		Post afternoon = new Post(null,sdf.parse("10/02/2019"),"Good afternoon","Hello friends good afternoon",new AuthorDTO(alex));
+		Post night = new Post(null,sdf.parse("10/10/2018"),"Nice nigth","Good night mi friends",new AuthorDTO(bob));
 		postRepository.saveAll(Arrays.asList(morning,afternoon,night));
 	}
 
