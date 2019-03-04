5e9dcbbde62e98b48e3bdda6dce3db8571619086
hmarinoo <hmarinoo@gmail.com>
2019-03-03 22:28:17 -0300

    Endpoint to return a user's posts

--- src/main/java/com/hmarinoo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/hmarinoo/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.hmarinoo.workshopmongo.domain.Post;
 import com.hmarinoo.workshopmongo.domain.User;
 import com.hmarinoo.workshopmongo.dto.UserDTO;
 import com.hmarinoo.workshopmongo.services.UserService;
@@ -59,5 +60,9 @@ public class UserResource {
 		user = userService.updateUser(user);
 		return ResponseEntity.noContent().build();
 	}
-
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
 }
